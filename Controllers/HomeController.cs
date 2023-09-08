24147e6bb22622f116293e708248429a6e98ff0e
Fallmaple8125 <hsyuch8125@gmail.com>
2023-09-09 02:05:15 +0800

    新增&移除頁面

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,23 +8,47 @@ namespace PaintingCommission.Controllers
 {
     public class HomeController : Controller
     {
+        #region 首頁
         public ActionResult Index()
         {
             return View();
         }
+        #endregion
 
-        public ActionResult About()
+        #region 繪師簡介
+        public ActionResult Profile()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
+        #endregion
 
-        public ActionResult Contact()
+        #region 作品集
+        public ActionResult Illustration()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
+        #endregion
+
+        #region 委託(+報價?)
+        public ActionResult Commission()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        #endregion
+
+        #region 排程(後端使用者顯示)
+        public ActionResult Schedule()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        #endregion
     }
 }
